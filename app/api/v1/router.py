d07d3bd162faa1b6abaf035c87f3fb94688545ba
손동민 <sondmmm0308@gmail.com>
2026-07-30 19:22:49 +0900

    [refactor] 사용하지 않는 파일 제거

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -7,7 +7,6 @@ from app.api.v1.endpoints import (
     member,
     model_answer,
     rubric,
-    student,
     answer_sheet,
     answer_region,
     ocr_result,
@@ -25,7 +24,6 @@ api_router.include_router(problem.router)
 api_router.include_router(member.router)
 api_router.include_router(model_answer.router)
 api_router.include_router(rubric.router)
-api_router.include_router(student.router)
 api_router.include_router(answer_sheet.router)
 api_router.include_router(answer_region.router)
 api_router.include_router(ocr_result.router)
